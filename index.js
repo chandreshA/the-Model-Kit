1382d963358b2e95bea1756e2d1b9097447590a9
Chandresh Ahir <cbahir1996@gmail.com>
2026-05-03 12:51:05 -0400

    integrate pexels api to get pictures for homepage

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const port = 3000;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const defaultCity = 'New York';
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+const PEXELS_API_URL = 'https://api.pexels.com/v1/search';
 
 app.use(express.static('public'));
 
@@ -66,6 +67,175 @@ function getShootSuggestion(description, temp) {
   };
 }
 
+// function to get random model images for the homepage hero section based on different fashion categories
+async function getHeroImages() {
+  const heroQueries = [
+    "editorial fashion model",
+    "male fashion model portrait",
+    "beauty model portrait",
+    "streetwear model",
+    "runway fashion model"
+  ];
+
+  const imageRequests = heroQueries.map((query) => {
+    return axios.get(PEXELS_API_URL, {
+      headers: {
+        Authorization: process.env.PEXELS_API_KEY
+      },
+      params: {
+        query: query,
+        per_page: 1,
+        page: Math.floor(Math.random() * 10) + 1,
+        orientation: "portrait"
+      }
+    });
+  });
+
+  const responses = await Promise.all(imageRequests);
+
+  return responses.map((response) => {
+    return response.data.photos[0];
+  });
+}
+
+// async function getFeaturedImages() {
+//   const featuredDirections = [
+//     {
+//       name: "Editorial",
+//       style: "editorial",
+//       query: "editorial fashion model"
+//     },
+//     {
+//       name: "Commercial",
+//       style: "commercial",
+//       query: "male commercial model portrait",
+//       type: "local",
+//       image: "/images/C_Ahir-36.jpeg",
+//       alt: "Commercial modeling inspiration"
+//     },
+//     {
+//       name: "Runway",
+//       style: "runway",
+//       query: "runway fashion model"
+//     },
+//     {
+//       name: "Streetwear",
+//       style: "streetwear",
+//       query: "streetwear model"
+//     },
+//     {
+//       name: "Beauty",
+//       style: "beauty",
+//       query: "beauty model portrait"
+//     },
+//     {
+//       name: "Fitness",
+//       style: "fitness",
+//       query: "fitness model"
+//     }
+//   ];
+
+//   const imageRequests = featuredDirections.map((direction) => {
+//     return axios.get(PEXELS_API_URL, {
+//       headers: {
+//         Authorization: process.env.PEXELS_API_KEY
+//       },
+//       params: {
+//         query: direction.query,
+//         per_page: 1,
+//         orientation: "portrait"
+//       }
+//     });
+//   });
+
+//   const responses = await Promise.all(imageRequests);
+
+//   return responses.map((response, index) => {
+//     const photo = response.data.photos[0];
+//     const direction = featuredDirections[index];
+
+//     return {
+//       name: direction.name,
+//       style: direction.style,
+//       image: photo.src.large,
+//       alt: photo.alt || `${direction.name} modeling inspiration`
+//     };
+//   });
+// }
+
+async function getFeaturedImages() {
+  const featuredDirections = [
+    {
+      name: "Editorial",
+      style: "editorial",
+      query: "editorial fashion model"
+    },
+    {
+      name: "Commercial",
+      style: "commercial",
+      query: "male commercial model portrait",
+      type: "local",
+      image: "/images/C_Ahir-36.jpeg",
+      alt: "Commercial modeling inspiration"
+    },
+    {
+      name: "Runway",
+      style: "runway",
+      query: "runway fashion model"
+    },
+    {
+      name: "Streetwear",
+      style: "streetwear",
+      query: "streetwear model"
+    },
+    {
+      name: "Beauty",
+      style: "beauty",
+      query: "beauty model portrait"
+    },
+    {
+      name: "Fitness",
+      style: "fitness",
+      query: "fitness model"
+    }
+  ];
+  const featuredImages = await Promise.all(
+    featuredDirections.map(async (direction) => {
+      if (direction.type === "local") {
+        return {
+          name: direction.name,
+          style: direction.style,
+          image: direction.image,
+          alt: direction.alt
+        };
+      }
+
+      const response = await axios.get(PEXELS_API_URL, {
+        headers: {
+          Authorization: process.env.PEXELS_API_KEY
+        },
+        params: {
+          query: direction.query,
+          per_page: 1,
+          orientation: "portrait"
+        }
+      });
+
+      const photo = response.data.photos[0];
+
+      return {
+        name: direction.name,
+        style: direction.style,
+        image: photo.src.large,
+        alt: photo.alt || `${direction.name} modeling inspiration`
+      };
+    })
+  );
+
+  return featuredImages;
+}
+
+
 
 app.get('/', async (req, res) => {
     try {
@@ -76,6 +246,8 @@ app.get('/', async (req, res) => {
                 appid: process.env.WEATHER_API_KEY
             }
         });
+        const heroResponse = await getHeroImages();
+        const featuredResponse = await getFeaturedImages();
         const description = response.data.weather[0].description;
         const temp = response.data.main.temp;
         const shootSuggestion = getShootSuggestion(description, temp);
@@ -85,7 +257,9 @@ app.get('/', async (req, res) => {
         res.render('index.ejs', { weatherData: response.data.weather[0].description, 
             city: response.data.name,
             temp: response.data.main.temp,
-            shootSuggestion: shootSuggestion
+            shootSuggestion: shootSuggestion,
+            heroImage: heroResponse,
+            featuredImages: featuredResponse
         });
     } catch (error) {
         console.error("Failed to fetch weather:", error.message);
